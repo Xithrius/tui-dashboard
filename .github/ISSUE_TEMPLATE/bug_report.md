34838159f1017da3fa9170c325c11662833a4cca
Xithrius <xithrius@gmail.com>
2023-04-12 03:57:44 -0700

    Added base of project

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,29 @@
+---
+name: Bug report
+about: Create a report to help us improve
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+**Describe the bug:**
+<!-- A clear description of what the bug is -->
+
+**To Reproduce:**
+<!-- Numbered steps to reproduce the behavior -->
+
+**Expected behavior:**
+<!-- Describe what you expect to happen -->
+
+**Screenshots:**
+<!-- Screenshots in context with the problem -->
+
+**System:**
+- tui-logger version:
+
+<!-- [e.g. Windows 10, Arch Linux w/ kernel 5.16.2] -->
+- Operating System:
+
+**Additional context:**
+Add any other context about the problem here.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
-pub fn hello_world() -> String {
-    "Hello, world!".to_string()
+use tui::{
+    style::Style,
+    text::{Spans, Text},
+};
+
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct Choice<T> {
+    key: T,
+    text: T,
+    bracket_style: Style,
 }
+
+impl<T> Choice<T> {
+    pub fn new(key: T, text: T, bracket_style: Style) -> Self {
+        Self {
+            key,
+            text,
+            bracket_style,
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct Section<T> {
+    title: Option<T>,
+    choices: Vec<Choice<T>>,
+}
+
+impl<T> Section<T> {
+    pub fn new(title: Option<T>, choices: Vec<Choice<T>>) -> Self {
+        Self { title, choices }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct TuiDashboardWidget<'a, T: Into<Text<'a>>> {
+    text: Vec<Spans<'a>>,
+    sections: Vec<Section<T>>,
+}
+
+// https://github.com/gin66/tui-logger/blob/master/src/lib.rs#L1120
+// impl Widget for TuiDashboardWidget<>
